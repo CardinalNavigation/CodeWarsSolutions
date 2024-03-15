d0a498c37d27828890b13853854c0769e2cad8c1
Matthew Johnson <mtthw.jhnson@gmail.com>
2024-03-15 11:07:35 -0500

    Next Problem Completed, no test suite added yet

--- 3.10.24/index.js
+++ 3.10.24/index.js
@@ -1,21 +1,41 @@
-/**In this kata you will create a function that takes a 
- * list of non-negative integers and strings and returns 
+/**In this kata you will create a function that takes a
+ * list of non-negative integers and strings and returns
  * a new list with the strings filtered out.
  */
 
 function filterList(l) {
   // Return a new array with the strings filtered out
-  let numberArray=[]
-  for (let i=0; i < l.length; i++){
-    if ((typeof l[i])=='number'){
-        numberArray.push(l[i])
+  let numberArray = [];
+  for (let i = 0; i < l.length; i++) {
+    if (typeof l[i] == "number") {
+      numberArray.push(l[i]);
     }
   }
-  return numberArray
+  return numberArray;
 }
 
 module.exports = filterList;
 
-
 // console.log(filter_list([1,2,'a','b']))
 
+/**
+ * Write an algorithm that takes an array and moves all of the zeros to the end,
+ * preserving the order of the other elements.
+ */
+
+// declare two new arrays, one for everything else, one for Zeros.
+// combine both arrays with the spread operator. 
+function moveZeros(arr) {
+  let zeroArray = [];
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeroArray.push(arr[i]);
+    } else {
+      newArray.push(arr[i]);
+    }
+  }
+  return [...newArray, ...zeroArray]
+}
+
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
--- 3.10.24/test.js
+++ 3.10.24/test.js
@@ -1,5 +1,5 @@
 const filterList = require("../3.10.24/index");
 
 test("filterList is filtering strings?", () => {
-    expect(filterList(["a", 1])).toBe([1])
+  expect(filterList(["a", 1])).toStrictEqual([1]);
 });
